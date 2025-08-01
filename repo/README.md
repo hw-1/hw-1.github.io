263a85a83210bb9f305d096e1b2595adaf0b6d6f
git <>
2025-08-01 11:39:55 +0800

    hw-1

--- repo/README.md
+++
@@ -1,2 +0,0 @@
-# Xinam1ne Repository
-Go to <a href="repo">Xinam1ne Repository</a>
--- repo/packages.sh
+++
@@ -1,7 +0,0 @@
-cd "`dirname \"$0\"`"
-REPOROOT=$(pwd)
-#echo $REPOROOT
-
-
-dpkg-scanpackages ./Files/ > Packages
-bzip2 -fks Packages
